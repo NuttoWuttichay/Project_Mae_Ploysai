e11c2cf355b440c56a58b6d59a150703f310b1b2
WuttichaiPolkuntod <65309010045@tatc.ac.th>
2023-06-05 14:08:38 +0700

    Work06_05_2023

--- Navbar.php
+++ Navbar.php
@@ -5,20 +5,20 @@
 
 <nav class="navbar navbar-expand-lg bg-Primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Mae Ploysai</a>
+    <a class="navbar-brand text-light" href="#">Mae Ploysai</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>   
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active text-light" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Menu</a>
+          <a class="nav-link text-light" href="Menu.php">Menu</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link text-light" href="About.php">About</a>
         </li>
       </ul>
         <button class="btn btn-outline-light" type="submit">Login Now</button>
