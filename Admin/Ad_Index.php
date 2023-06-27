be0e3f5e2c5ca24121f06329917f644c8c4e85fc
WuttichaiPolkuntod <65309010045@tatc.ac.th>
2023-06-27 12:58:37 +0700

    Ad_Nav

--- Admin/Ad_Index.php
+++ Admin/Ad_Index.php
@@ -10,7 +10,7 @@
     <div class="header">
         <div class="side-nav">
             <a href="#" class="logo">
-                <img src="logo.png" class="logo-img">
+                <img src="Ad_image/Logo_mae_Ploysai.png" class="logo-img">
             </a>
             <ul class="nav-links">
                 <li><a href="#"><i class="fa-regular fa-house" style="color: #ffffff;"></i><p>Dashboard</p></a></li>
@@ -21,6 +21,7 @@
                 <li><a href="#"><p>Order</p></a></li>
                 <li><a href="#"><p>Order History</p></a></li>
                 <li><a href="#"><p>Logout</p></a></li>
+                <div class="active"></div>
             </ul>
         </div>
     </div>
