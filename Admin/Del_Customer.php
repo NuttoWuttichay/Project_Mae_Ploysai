bc7afc50a5407e6511f0bd252a21725604a50705
WuttichaiPolkuntod <65309010045@tatc.ac.th>
2023-07-07 14:24:16 +0700

    Del Fix

--- Admin/Del_Customer.php
+++ Admin/Del_Customer.php
@@ -1,8 +1,9 @@
 <?php
   require_once 'Ad_Customer.php';
   require_once 'Ad_Connect.php';
-  $cust_id = $_GET['Cust_id'];
-  $sql = "SELECT * FROM customer WHERE Cust_id='$cust_id'";
+  $cust_id = $_GET['Cust_ID'];
+  echo $cust_id;
+  $sql = "SELECT * FROM customer WHERE Cust_id ='$cust_id'";
   $result = $con->query($sql);
   $row = mysqli_fetch_array($result);
   // @unlink('pro_pic/'.$row['pro_pic']);
@@ -11,6 +12,6 @@
   if (!$result) {
     echo "<script>alert('ไม่สามารถลบข้อมูลได้')</script>";
   } else {
-    // echo "<script>window.location.href='Ad_Customer.php'</script>";
+    echo "<script>window.location.href='Ad_Customer.php'</script>";
   }
 ?>
