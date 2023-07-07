b7714d1b177fe2f60712fd2239a98ef2e7f631f8
WuttichaiPolkuntod <65309010045@tatc.ac.th>
2023-07-07 10:55:01 +0700

    Customer list

--- Admin/Ad_Customer.php
+++ Admin/Ad_Customer.php
@@ -1,3 +1,9 @@
+<?php
+    include 'Ad_Connect.php';
+    $sql="SELECT * FROM Customer";
+    $result=$con->query($sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,9 +27,48 @@
             ?>
             </div>
             <div class=" col-10 bg-danger h-100">
-                <div class="Page-Content">
-
-                </div>
+                <div class="Page-Content m-3">
+                    <h1 class="text-light">Customer</h1>
+                    <?php
+                    $counter=1;
+                    while($row=mysqli_fetch_array($result)){
+                    ?>
+                        <div class="row my-3 mx-1">
+                            <div class="card" style="width: 100%; height: 120px">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <img src="Ad_image/fortest.jpg" width="150" class="rounded float-start" alt="...">
+                                        </div>
+                                        <div class="col">
+                                            <p><?php echo $row['Cust_Firstname'] ?>&nbsp;&nbsp;<?php echo $row['Cust_Lastname'] ?></p>
+                                        </div>
+                                        <div class="col">
+                                            <?php echo $row['Cust_Email'] ?>
+                                        </div>
+                                        <div class="col">
+                                            <?php echo $row['Cust_Tel'] ?>
+                                        </div>
+                                        <div class="col">
+                                            <?php echo $row['Cust_Address'] ?>
+                                        </div>
+                                        <div class="col">
+                                            
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="row m-1">
+                                                <a href="#" class="btn btn-warning "><i class="bi bi-wrench"></i></a>
+                                            </div>
+                                            <div class="row m-1">
+                                                <a href="#" class="btn btn-danger"><i class="bi bi-trash"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <?php } ?>
+                </div>  
             </div>
         </div>
     </div>
--- Admin/Ad_Navbar.php
+++ Admin/Ad_Navbar.php
@@ -1,3 +1,4 @@
+
 <!-- Bootstrap -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
@@ -5,7 +6,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
     <!-- CSS Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
